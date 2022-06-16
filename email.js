eb57608279628e93bdf7508e511000ff75890dba
thedrivendev <64045761+thedrivendev@users.noreply.github.com>
2022-06-16 07:12:53 -0400

    Created email function, restructured handler

---
+++ email.js
@@ -0,0 +1,146 @@
+const AWS = require('aws-sdk');
+const SES = new AWS.SES();
+
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const mimemessage = require('mimemessage');
+
+const s3Client = new S3Client({ region: 'us-east-1' });
+
+const getS3Object = ({ s3Client, Bucket, Key }) =>
+  new Promise(async (resolve, reject) => {
+    const getObjectCommand = new GetObjectCommand({ Bucket, Key });
+
+    try {
+      const response = await s3Client.send(getObjectCommand);
+
+      // Store all of data chunks returned from the response data stream
+      // into an array then use Array#join() to use the returned contents as a String
+      let responseDataChunks = [];
+
+      // Handle an error while streaming the response body
+      response.Body.once('error', err => reject(err));
+
+      // Attach a 'data' listener to add the chunks of data to our array
+      // Each chunk is a Buffer instance
+      response.Body.on('data', chunk => responseDataChunks.push(chunk));
+
+      // Once the stream has no more data, join the chunks into a single buffer and return buffer
+      response.Body.once('end', () =>
+        resolve(Buffer.concat(responseDataChunks))
+      );
+    } catch (err) {
+      // Handle the error or throw
+      return reject(err);
+    }
+  });
+
+module.exports = async event => {
+  // (async event => {
+  // // Get file from S3
+  // const file = await s3Client.send(
+  //   new GetObjectCommand({
+  //     Bucket: 'farbankexport',
+  //     Key: 'inventory.xlsx',
+  //   })
+  // );
+
+  // Get file from S3
+  const fileData = await getS3Object({
+    s3Client,
+    Bucket: 'farbankexport',
+    Key: 'inventory.xlsx',
+  });
+  // // console.log('fileData');
+  // // console.log(Object.keys(file));
+  // // console.log(fileData);
+  // console.log(typeof fileData);
+
+  const fileBody = fileData.toString('base64');
+  // console.log('typeof fileBody');
+  // console.log(typeof fileBody);
+
+  // Prepare email
+  const mailContent = mimemessage.factory({
+    contentType: 'multipart/mixed',
+    body: [],
+  });
+
+  mailContent.header('From', 'thomas@getakamai.com');
+  mailContent.header('To', process.env.recipient);
+  mailContent.header('Subject', 'Farbank Inventory Export');
+
+  const alternateEntity = mimemessage.factory({
+    contentType: 'multipart/alternative',
+    body: [],
+  });
+
+  const htmlEntity = mimemessage.factory({
+    contentType: 'text/html;charset=utf-8',
+    body:
+      '<html>' +
+      ' <head></head>' +
+      ' <body>' +
+      '   <h1>Farbank Inventory Export</h1>' +
+      '   <p>Please see the attached file for current Farbank Inventory levels.</p>' +
+      ' </body>' +
+      '</html>' +
+      '' +
+      '',
+  });
+  const plainEntity = mimemessage.factory({
+    body: `Please see the attached file for current Farbank Inventory levels.
+    `,
+  });
+  alternateEntity.body.push(plainEntity);
+  alternateEntity.body.push(htmlEntity);
+
+  mailContent.body.push(alternateEntity);
+
+  // Set attachment
+  const attachmentEntity = mimemessage.factory({
+    contentType: 'text/plain',
+    contentTransferEncoding: 'base64',
+    body: fileBody,
+  });
+  attachmentEntity.header(
+    'Content-Disposition',
+    'attachment; filename="inventory.xlsx"'
+  );
+  mailContent.body.push(attachmentEntity);
+
+  const mailData = mailContent.toString();
+  // console.log(mailData);
+  // console.log(typeof mailData);
+
+  // Send email
+  // const sendEmailResponse = await sesClient
+  //   .send(
+  //     new SendEmailCommand({
+  //       Content: {
+  //         Raw: {
+  //           Data: Buffer.from(Data).toString('base64'),
+  //         },
+  //       },
+  //     })
+  //   )
+  //   .catch(error => console.log(error));
+
+  // console.log(sendEmailResponse.$metadata);
+
+  const response = await SES.sendRawEmail({
+    RawMessage: { Data: mailData },
+  }).promise();
+  console.log(response);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Farbank Inventory Export Emailed',
+      },
+      null,
+      2
+    ),
+  };
+};
+// })();
---
+++ export.js
@@ -0,0 +1,76 @@
+const chromium = require('chrome-aws-lambda');
+const fs = require('fs').promises;
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const s3Client = new S3Client({ region: 'us-east-1' });
+
+module.exports = async event => {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+
+  const [page] = await browser.pages();
+  await page.setViewport({ width: 1280, height: 800 });
+
+  await page._client.send('Page.setDownloadBehavior', {
+    behavior: 'allow',
+    // downloadPath: './downloads',
+    downloadPath: '/tmp/',
+  });
+
+  // Authenticate
+  await page.goto('https://orders.farbank.com');
+  await page.type('#Input_UserName', process.env.username);
+  await page.type('#Input_Password', process.env.password);
+  await page.click('button.btn-success');
+  await page.waitForTimeout(500);
+  // await page.waitForNetworkIdle();
+
+  const downloadUrl = 'https://orders.farbank.com/InventoryExport';
+
+  await page
+    .goto(downloadUrl, { waitUntil: 'networkidle0' })
+    .catch(error => console.log(error));
+
+  await page.waitForTimeout(500);
+  browser.close();
+
+  // const files = await fs.readdir('downloads/');
+  const files = await fs.readdir('/tmp/');
+  const file = files.find(file => file.includes('Inventory_'));
+  const fileData = await fs.readFile(`/tmp/${file}`);
+
+  const timestamp = new Date(Date.now()).toISOString().replace(/:/g, '-');
+
+  try {
+    const results = await s3Client.send(
+      new PutObjectCommand({
+        Bucket: 'farbankexport', // The name of the bucket. For example, 'sample_bucket_101'.
+        Key: `inventory.xlsx`, // The name of the object. For example, 'sample_upload.txt'.
+        Body: fileData, // The content of the object. For example, 'Hello world!".
+        ACL: 'public-read',
+      })
+    );
+    console.log(`Write file to S3: ${results['$metadata'].httpStatusCode}`);
+  } catch (err) {
+    console.log('Error', err);
+  }
+
+  // await fs.rm(`/tmp/${file}`);
+  // console.log(`File ${file} removed.`);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Farbank EOS Inventory Export Completed',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
