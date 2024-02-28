380974cb9079d8f37171c4ab49d98aae8c76ab4f
thedrivendev <64045761+thedrivendev@users.noreply.github.com>
2024-02-27 19:17:51 -0500

    added write to s3

--- getInventory.js
+++ getInventory.js
@@ -1,25 +1,28 @@
 const { wcGet } = require('./integrations/woocommerce');
 const XLSX = require('xlsx');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const s3Client = new S3Client({ region: 'us-east-1' });
 
 const getInventoryData = async () => {
   let inventoryData = [];
   const { data: products } = await wcGet(`products?per_page=100&status=publish&page=1&category=210,33`);
-  console.log(products.length);
-  // console.log(products);
-  // console.log(products.map(product => product.name));
-  // process.exit();
 
-  for (const product of products.filter(product => !product.name.includes('Sale') && !product.name.includes('Seconds'))) {
+  const filteredProducts = products.filter(product => !product.name.includes('Sale') && !product.name.includes('Seconds'));
+
+  let counter = 0;
+  const total = filteredProducts.length;
+  for (const product of filteredProducts) {
+    console.log(`Processing ${++counter}/${total}: ${product.name}`);
     const { data: productVariations } = await wcGet(`products/${product.id}/variations?per_page=100`);
-    console.log(productVariations[0]);
+
     for (const productVariation of productVariations) {
       const { sku, stock_quantity: quantity } = productVariation;
       const title = product.name + ' ' + productVariation?.attributes.map(attribute => attribute?.option).join(' ');
       inventoryData = [...inventoryData, { sku, title, quantity }];
     }
-    console.log(inventoryData);
   }
-  process.exit();
+  // console.log(inventoryData);
+  return inventoryData;
 };
 
 (async () => {
@@ -27,5 +30,19 @@ const getInventoryData = async () => {
   const workbook = XLSX.utils.book_new();
   const worksheet = XLSX.utils.json_to_sheet(inventoryData);
   XLSX.utils.book_append_sheet(workbook, worksheet, 'Inventory');
-  XLSX.write(wb, { type: 'buffer', bookType: 'xlsx' });
+  const fileData = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' });
+
+  try {
+    const results = await s3Client.send(
+      new PutObjectCommand({
+        Bucket: 'winstonexport', // The name of the bucket. For example, 'sample_bucket_101'.
+        Key: `inventory.xlsx`, // The name of the object. For example, 'sample_upload.txt'.
+        Body: fileData, // The content of the object. For example, 'Hello world!".
+        ACL: 'public-read',
+      })
+    );
+    console.log(`Write file to S3: ${results['$metadata'].httpStatusCode}`);
+  } catch (err) {
+    console.log('Error', err);
+  }
 })();
